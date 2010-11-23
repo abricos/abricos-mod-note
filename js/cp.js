83a48e4a422c2795871b0c049340a84d97d5bcde
Alexander Kuzmin <roosit@abricos.org>
2010-11-23 10:40:28 +0000

    Модуль "Блокнот": добавлены языковые фразы для ролей

---
+++ js/cp.js
@@ -0,0 +1,11 @@
+/*
+@version $Id: cp.js 813 2010-11-23 09:10:02Z roosit $
+@copyright Copyright (C) 2008 Abricos All rights reserved.
+@license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
+
+var Component = new Brick.Component();
+Component.requires = {
+	mod:[{name: 'user', files: ['cpanel.js']}]
+};
+Component.entryPoint = function(){};
